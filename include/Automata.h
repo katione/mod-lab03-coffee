58132b2e773dc97d78c014717e39278649258e85
katione <147106388+katione@users.noreply.github.com>
2024-03-31 02:31:24 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,32 @@
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
+
+#include <vector>
+#include <string>
+
+enum STATES {OFF,WAIT,ACCEPT,CHECK,COOK};
+
+class Automata {
+ private:
+    int cash;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    STATES state;
+    bool check(int);
+    void cook(int);
+    void finish(int);
+
+ public:
+    Automata();
+    ~Automata();
+    void on();
+    void off();
+    void coin(int);
+    void getMenu();
+    STATES getState();
+    void choice(int);
+    void cancel();
+};
+
+#endif
 
