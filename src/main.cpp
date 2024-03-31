f358626d783d1a4e1b8f04f7b252c1fca25a8f43
katione <147106388+katione@users.noreply.github.com>
2024-03-31 03:27:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "Automata.h"
 
+int main() {
+  Automata CoffeMachine
+  CoffeMachine.on();
+  CoffeMachine.getMenu();
+  CoffeMachine.coin(100);
+  CoffeMachine.choice(8);
+  std::cout << std::endl;
+
+  std::cout << std::endl;
+  CoffeMachine.coin(100);
+  CoffeMachine.choice(7);
+  CoffeMachine.coin(25);
+  CoffeMachine.choice(7);
+
+  std::cout << std::endl;
+  CoffeMachine.cancel();
+  std::cout << CoffeeMachine.getState() << std::endl;
+  CoffeMachine.off();
+  return 0;
+}
